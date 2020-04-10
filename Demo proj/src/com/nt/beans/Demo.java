848d53050c569b818b903c96a2b1aacd88447555
Dev2 <Dev2@gmail.com>
2020-04-10 23:20:39 +0530

    vzd

--- Demo proj/src/com/nt/beans/Demo.java
+++ Demo proj/src/com/nt/beans/Demo.java
@@ -2,7 +2,7 @@ package com.nt.beans;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("hiii");
+		System.out.println("hiiggggi");
 		System.out.println("hiiiiiii");
 	}
 
